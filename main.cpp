7d0c4478ae6122e3e9791752a8d83818566cfebd
dinaraparanid <dinaraparanid@gmail.com>
2023-09-24 13:54:52 +0300

    tests

--- main.cpp
+++ main.cpp
@@ -2,9 +2,11 @@
 
 #include "Simplex.h"
 #include "simplex_utils.h"
+#include "tests.h"
 
-int main() {
+int main(int argc, char** argv) {
     Simplex answer = perform_simplex_method();
     std::cout << answer;
-    return 0;
+
+    return run_tests(argc, argv);
 }
